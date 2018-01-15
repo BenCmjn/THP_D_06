13c5d6f6caa4e58cecb29c9bd42e59f26719ea6e
BenCmjn <benjamin.calmejane@gmail.com>
2018-01-15 19:21:50 +0100

    Gros commentaires

--- stock_picker.rb
+++ stock_picker.rb
@@ -14,37 +14,43 @@
 # ex pour [17,3,6,9,15,8,6,1,10] on va choisir 3 (jour 2) pour l'achat et et 15 (jour 5) pour la revente
 #affiche le jour d'achat, de revente puis le prix de revente - le prix d'achat = le benefice
 
-
-def trader_du_dimanche(prix)    
-    #initialisation des 4 parametres
+# Déclaration de la méthode avec "prix" en paramètre
+def trader_du_dimanche(prix)
+    #initialisation des 4 variables
     jourdachat = 0
     jourdevente = 0
     benefice = 0
     benefice_temp = 0
+    # Correction de la taille du tableau pour l'exploiter dans une boucle for
     taille_array = prix.length - 1
+    # Annonce du spectre de la comparaison des prix (du 1er au dernier jour)
     puts "\n Comparaison sur #{taille_array} Jours :"
     
-    #initialisation param
-    for i in (0..taille_array) #boucle jour achat
-        for j in (i+1..taille_array) #boucle jour arrivée
+    # La boucle "i" va passer en revue tous les jours depuis le début
+    for i in (0..taille_array) # début boucle jour meilleur achat
+        # Puis la boucle "j" va passer en revue tous les jours depuis "i"
+        for j in (i+1..taille_array) # début boucle jour meilleur vente
             
-            benefice_temp = prix[j] - prix[i]
+            benefice_temp = prix[j] - prix[i] # à chaque tour on calcule la différence
 
-            if benefice_temp > benefice
-                jourdachat = i 
-                jourdevente = j 
-                benefice = benefice_temp
-            end
-        end
-    end
+            if benefice_temp > benefice # Si le bénef est plus grand qu'actuellement,
+                jourdachat = i # On retient le prix d'achat
+                jourdevente = j #On retient le prix de vente
+                benefice = benefice_temp # On met à jour le bénéfice
+
+            end #Et on recommence jusqu'à la fin du tableau
+        end # fin boucle jour meilleur vente (j)
+    end# fin boucle jour meilleur achat (i)
+
+# On affiche toute ces résultats
 puts "\n Meilleur jour pour l'achat : #{jourdachat+1} -> $#{prix[jourdachat]}
       \n Meilleur jour pour la vente: #{jourdevente+1} -> $#{prix[jourdevente]}
       \n Bénéfice sur la semaine : $#{benefice}"
+end # Fin de la méthode
 
-end
-
+#Appel de la méthode, pour le tableau de prix suivant :
 trader_du_dimanche([17,3,6,9,15,8,6,1,10])
-puts
+puts # Passe une ligne avant la fin du programme.
 
 
 
