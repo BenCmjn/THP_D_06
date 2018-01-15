160654a7cdcc064918f02ec67c376a04eb5021ca
BenCmjn <benjamin.calmejane@gmail.com>
2018-01-15 15:31:13 +0100

    modif de cesar.rb, ça marche

--- cesar.rb
+++ cesar.rb
@@ -2,9 +2,19 @@
 # qui prend en paramètre un string et le nombre de lettre
 # à décaler, et sort le string modifié.
 
-def chiffre_de_cesar("chose", x)
+def code_de_cesar(phrase)
+	phrase.each_char do |lettre|
+		lettre = lettre.ord
+		lettre += 3
+		lettre = lettre.chr
+		print lettre
+	end
 	
 end
 
- chiffre_de_cesar("What a string!", 5)
+# chiffre_de_cesar("What a string!", 5)
 # => "Bmfy f xywnsl!"
+
+
+code_de_cesar("Ceci est un code à chiffrer")
+puts
